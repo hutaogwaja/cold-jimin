aa7f83f16d730f96bbe9c33aa4b1259c28b0559c
hutaogwaja <hutaogwaja@gmail.com>
2026-07-25 08:11:04 +0900

    changed invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,11 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { SlashCommandBuilder } from 'discord.js';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, '../config.json'), 'utf-8'));
 
 export default {
     // 슬래시 명령어 메타데이터 정의
@@ -8,6 +15,6 @@ export default {
 
     // 명령어가 실행되었을 때 동작
     async execute(interaction) {
-        await interaction.reply(`[이거를 누르면 초대할 수 있어요!](https://discord.com/oauth2/authorize?client_id=1529896046380187719&permissions=8&integration_type=0&scope=bot)`);
+        await interaction.reply(`[이거를 누르면 초대할 수 있어요!](https://discord.com/oauth2/authorize?client_id=${config.clientID}&permissions=8&integration_type=0&scope=bot)`);
     },
 };
